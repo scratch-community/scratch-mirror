1ab42319057894b837c2ce05f54a65eadae8d786
apricotwtf <apricotwtf@users.noreply.github.com>
2024-01-24 01:26:40 +0000

    Deploying to gh-pages from @ scratch-community/scratch-mirror@2de4e7025fb2b4453d1c78d76a9c3c3572eec5c3 🚀

---
+++ tw/4a2eac7041f0d91ef86f75879258be5b365e8862/js/addon-entry-tw-remove-feedback.5176dddfd7d91dafd287.js
@@ -0,0 +1,1 @@
+(window.webpackJsonpGUI=window.webpackJsonpGUI||[]).push([[57],{1589:function(n,e,s){"use strict";s.r(e),s.d(e,"resources",(function(){return o}));var c=s(1590);const o={"remove-feedback.css":s.n(c).a}},1590:function(n,e,s){(n.exports=s(9)(!1)).push([n.i,'a[class^="menu-bar_feedback-link_"] {\n  display: none;\n}\n',""])}}]);
